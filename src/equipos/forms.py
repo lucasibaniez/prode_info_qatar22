fac04592eed95be6d8793766a0127ec820395960
lucas <lucas.sebib@egmail.com>
2022-07-25 21:16:14 -0300

    errores en forms

--- src/equipos/forms.py
+++ src/equipos/forms.py
@@ -4,6 +4,15 @@ from django import forms
 from .models import Equipo
 
 class EquipoForm(forms.ModelForm):
+    nombre=forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    # cantidad_jugadores=forms.IntegerField()
+
     class  Meta:
         model = Equipo
-        fields = ["nombre"] 
+        fields = ["nombre"]
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data["nombre"]
+        if nombre[0] != "A":
+            raise forms.ValidationError("El nombre del equipo debe iniciar con la letra A")
+        return nombre
