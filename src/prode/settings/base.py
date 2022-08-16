fd5011d83c2b9ee6319e04d9de6c923f51d499af
lucas <lucas.sebib@egmail.com>
2022-08-16 19:26:39 -0300

    agregar usuario en admin

--- src/prode/settings/base.py
+++ src/prode/settings/base.py
@@ -77,9 +77,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
--- src/usuarios/admin.py
+++ src/usuarios/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from .models import Usuario
 
 # Register your models here.
-admin.site.register(Usuario)
+# admin.site.register(Usuario)
+
+
+@admin.register(Usuario)
+class UserAdmin(DjangoUserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    
+    list_display = ['id', 'username', 'email', 'first_name', 'last_name']
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    ordering = ('username',)
